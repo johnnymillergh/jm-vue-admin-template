6a4ae10fe02f46469c6dac22145f48925bd4bff8
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2019-04-06 14:32:01 +0800

    feat($Build): add new components
    1. Heading
    2. Horizontal Separator

---
+++ src/components/Heading/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-row class="heading-container">
+    <h1 :class="'heading-container-level'+level">{{ text }}</h1>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'Heading',
+      required: true
+    },
+    level: {
+      type: Number,
+      default: 1,
+      required: false
+    }
+  },
+  name: 'Heading',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.heading-container {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 10px;
+}
+
+.heading-container-level1 {
+  color: #409eff;
+  font-size: 18px;
+  line-height: 16px;
+  font-weight: bolder;
+  border-left: 4px solid #409eff;
+  padding-left: 16px;
+  margin: 8px 16px 8px 0;
+}
+
+.heading-container-level2 {
+  color: #409eff;
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: bolder;
+  border-left: 4px solid #409eff;
+  padding-left: 16px;
+  margin: 8px 16px 8px 0;
+}
+</style>
---
+++ src/components/HorizontalSeparator/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <hr :class="separatorStyle">
+</template>
+
+<script>
+export default {
+  name: 'LineSeparator',
+  props: {
+    separatorStyle: {
+      type: String,
+      default: 'gradient',
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+hr.normal {
+  border: 0;
+  height: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+hr.gradient {
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(119, 119, 127, 0.75), rgba(0, 0, 0, 0));
+}
+</style>
