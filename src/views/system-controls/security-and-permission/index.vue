22952f395ee0f5d3df708e6faee64ad43d97acbb
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2019-04-12 13:15:10 +0800

    feat($UI): add tab transition animation

--- src/views/system-controls/security-and-permission/index.vue
+++ src/views/system-controls/security-and-permission/index.vue
@@ -1,75 +1,87 @@
 <template>
   <div class="app-container">
-    <el-tabs @tab-click="handleClick" type="border-card">
+    <el-tabs @tab-click="onTabClick" type="border-card">
       <el-tab-pane label="Permission Controls">
-        <heading text="API Glance"/>
-        <el-row :gutter="5">
-          <el-col :span="8">
-            <el-card shadow="never" style="height: 290px" :body-style="{padding: '0'}">
-              <div slot="header" class="clearfix">
-                <div style="margin-bottom: 5px">Controller List</div>
-                <el-input v-model="searchText" size="mini" placeholder="Type class name to search"/>
-              </div>
-              <div style="height: 202px; overflow: auto; padding: 5px">
-                <el-table :data="getFilteredControllerList()"
-                          stripe
-                          highlight-current-row
-                          @row-click="onClickTableRow"
-                          v-loading="controllerListLoading">
-                  <el-table-column label="#" width="50px">
-                    <template slot-scope="scope">
-                      <el-radio v-model="selectedController" :label="scope.row" @change="onSelectController"><i></i>
-                      </el-radio>
-                    </template>
-                  </el-table-column>
-                  <el-table-column prop="className" label="Class Name"/>
-                </el-table>
-              </div>
-            </el-card>
-          </el-col>
-          <el-col :span="16">
-            <el-card shadow="never" style="height: 290px">
-              <el-form label-width="120px" v-loading="apiSelectFormLoading">
-                <el-form-item label="Status">
-                  <el-radio-group v-model="apiStatus" @change="onChangeApiStatus">
-                    <el-radio :label="ApiStatus.IDLED.status" border>Idled {{ getIdledApiCount() }}</el-radio>
-                    <el-radio :label="ApiStatus.IN_USED.status" border>In use {{ getInUseApiCount() }}</el-radio>
-                  </el-radio-group>
-                </el-form-item>
-                <el-form-item label="URL">
-                  <el-select v-model="selectedUrl" @change="onSelectUrl" placeholder="Select" style="width: 598px">
-                    <el-option v-for="(item,index) in apiList" :label="item.url" :value="index" :key="index"/>
-                  </el-select>
-                </el-form-item>
-                <el-form-item label="Method">
-                  <el-input v-model="method" readonly/>
-                </el-form-item>
-                <el-form-item label="Description">
-                  <el-input v-model="description" readonly/>
-                </el-form-item>
-              </el-form>
-            </el-card>
-          </el-col>
-        </el-row>
-        <el-row style="margin-top: 10px; margin-bottom: 5px">
-          <el-col align="right">
-            <el-button type="warning">Set it in Use</el-button>
-          </el-col>
-        </el-row>
-        <heading text="API Analysis"/>
-        <el-row style="height: 150px; background: #409EFF">
-          Pie Chart for API Analysis Here.
-        </el-row>
-        <heading text="API Details"/>
-        <el-row style="height: 450px; background: #409EFF">
-          API detail table here.
-        </el-row>
+        <transition name="el-zoom-in-top">
+          <div v-show="showPermissionControls">
+            <heading text="API Glance"/>
+            <el-row :gutter="5">
+              <el-col :span="8">
+                <el-card shadow="never" style="height: 290px" :body-style="{padding: '0'}">
+                  <div slot="header" class="clearfix">
+                    <div style="margin-bottom: 5px">Controller List</div>
+                    <el-input v-model="searchText" size="mini" placeholder="Type class name to search"/>
+                  </div>
+                  <div style="height: 202px; overflow: auto; padding: 5px">
+                    <el-table :data="getFilteredControllerList()"
+                              stripe
+                              highlight-current-row
+                              @row-click="onClickTableRow"
+                              v-loading="controllerListLoading">
+                      <el-table-column label="#" width="50px">
+                        <template slot-scope="scope">
+                          <el-radio v-model="selectedController" :label="scope.row" @change="onSelectController"><i></i>
+                          </el-radio>
+                        </template>
+                      </el-table-column>
+                      <el-table-column prop="className" label="Class Name"/>
+                    </el-table>
+                  </div>
+                </el-card>
+              </el-col>
+              <el-col :span="16">
+                <el-card shadow="never" style="height: 290px">
+                  <el-form label-width="120px" v-loading="apiSelectFormLoading">
+                    <el-form-item label="Status">
+                      <el-radio-group v-model="apiStatus" @change="onChangeApiStatus">
+                        <el-radio :label="ApiStatus.IDLED.status" border>Idled {{ getIdledApiCount() }}</el-radio>
+                        <el-radio :label="ApiStatus.IN_USED.status" border>In use {{ getInUseApiCount() }}</el-radio>
+                      </el-radio-group>
+                    </el-form-item>
+                    <el-form-item label="URL">
+                      <el-select v-model="selectedUrl" @change="onSelectUrl" placeholder="Select" style="width: 598px">
+                        <el-option v-for="(item,index) in apiList" :label="item.url" :value="index" :key="index"/>
+                      </el-select>
+                    </el-form-item>
+                    <el-form-item label="Method">
+                      <el-input v-model="method" readonly/>
+                    </el-form-item>
+                    <el-form-item label="Description">
+                      <el-input v-model="description" readonly/>
+                    </el-form-item>
+                  </el-form>
+                </el-card>
+              </el-col>
+            </el-row>
+            <el-row style="margin-top: 10px; margin-bottom: 5px">
+              <el-col align="right">
+                <el-button type="warning">Set it in Use</el-button>
+              </el-col>
+            </el-row>
+            <heading text="API Analysis"/>
+            <el-row style="height: 150px; background: #409EFF">
+              Pie Chart for API Analysis Here.
+            </el-row>
+            <heading text="API Details"/>
+            <el-row style="height: 450px; background: #409EFF">
+              API detail table here.
+            </el-row>
+          </div>
+        </transition>
       </el-tab-pane>
       <el-tab-pane label="Role Controls">
-        Role Controls
+        <transition name="el-zoom-in-top">
+          <div v-show="showRoleControls">
+            Role Controls
+          </div>
+        </transition>
       </el-tab-pane>
       <el-tab-pane label="Authorization">
-        Authorization
+        <transition name="el-zoom-in-top">
+          <div v-show="showAuthorization">
+            Authorization
+          </div>
+        </transition>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -90,6 +102,9 @@ export default {
   },
   data () {
     return {
+      showPermissionControls: true,
+      showRoleControls: false,
+      showAuthorization: false,
       controllerListLoading: false,
       apiSelectFormLoading: false,
       searchText: null,
@@ -106,10 +121,26 @@ export default {
     }
   },
   methods: {
+    onTabClick (tab) {
+      switch (tab.index) {
+        case '0':
+          this.showPermissionControls = true
+          this.showRoleControls = this.showAuthorization = false
+          break
+        case '1':
+          this.showRoleControls = true
+          this.showPermissionControls = this.showAuthorization = false
+          break
+        case '2':
+          this.showAuthorization = true
+          this.showRoleControls = this.showPermissionControls = false
+          break
+      }
+    },
     getController () {
       this.controllerListLoading = true
       SecurityAndPermission.getController().then(response => {
-        this.controllerList = response.data
+        this.controllerList = response.data.controllerList
       }).catch(error => {
         console.error(error)
         this.$message.error(error)
@@ -123,8 +154,6 @@ export default {
       }
       return this.controllerList.filter(data => !this.searchText ||
         data.className.toLowerCase().includes(this.searchText.toLowerCase()))
-    },
-    handleClick () {
     },
     onClickTableRow (row) {
       this.selectedController = row
